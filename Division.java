48759b472f4218455e7baf3ec7f1b4194a05a4d5
unknown <jgonzalo.hernandezy@gmail.com>
2024-05-05 21:41:39 -0600

    division de 2 numeros

--- Division.java
+++ Division.java
@@ -4,7 +4,7 @@ public class Division {
     int a, b, r;
 
     // Se declara el metodo
-    public int dividir(int a, int b) {
+    public int dividir() {
         // Se llama al objeto
         r = this.a / this.b;
        
@@ -12,4 +12,33 @@ public class Division {
         // Se cierra el metodo dividir
     }
     // Se cierra la clase
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "Division [a=" + a + ", b=" + b + ", r=" + r + "]";
+    }
 }
--- MainDivision.java
+++ MainDivision.java
@@ -12,19 +12,18 @@ public class MainDivision {
         Division d1 = new Division();
         System.out.println("Ingresa un numero");
         // Se almacena el valor del usuario
-        int a = n1.nextInt();
+        d1.setA(n1.nextInt());
         // Se almacena el valor del usuario
         System.out.println("Ingrese el segundo numero");
         // Se almacena el valor del usuario
-        int b = n1.nextInt();
-        // Se hace la operacion con los datos ingresados por el usuario
-        int r = a / b;
+        d1.setB(n1.nextInt());
         // Se imprime el resultado de la operacion y se muestra en pantalla
-        d1.a=a;
-        d1.b=b;
-        d1.dividir(a, b);
-        System.out.println("el resultado es :"+ d1.dividir(a, b));
+        System.out.println("el resultado es : ");
+        System.out.println(d1.dividir());
+        System.out.println(d1);
 
+        d1.getA();
+        d1.getB();
         // Se cierra el metodo mian
     }
     // Se cierra clase
